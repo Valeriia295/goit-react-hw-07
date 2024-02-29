87378bf63db8e82b5b6dbceb6c749b52220024fc
Valeriia295 <sokolsokol295@gmail.com>
2024-02-29 01:35:33 +0100

    Update of App

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import ContactForm from './components/ContactForm/ContactForm';
 import SearchBox from './components/SearchBox/SearchBox';
 import ContactList from './components/ContactList/ContactList';
 import css from './App.module.css';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchContacts } from './redux/operations';
 import { useEffect } from 'react';
 
 export default function App() {
   const dispatch = useDispatch();
+  const loading = useSelector(state => state.contacts.loading);
+  const error = useSelector(state => state.contacts.error);
 
   useEffect(() => {
     dispatch(fetchContacts());
@@ -15,6 +17,8 @@ export default function App() {
 
   return (
     <div className={css.container}>
+      {loading && <p className={css.loading}>Loading...Please wait</p>}
+      {error && <p className={css.text}>Oops!Something went wrong, please try again later.</p>}
       <h1 className={css.title}>
         Phone<span className={css.span}>book</span>
       </h1>
